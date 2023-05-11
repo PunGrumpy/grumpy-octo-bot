b15722458027ef9c55856e648985a69e5bd610f0
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-12 01:07:18 +0700

    :package: update all

---
+++ pino.d.ts
@@ -0,0 +1,4 @@
+declare module 'pino' {
+  var pino: any
+  export = pino
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
 import { Probot } from 'probot'
 import app from './app'
 
-const probot = new Probot({
-  id: process.env.APP_ID,
-  secret: process.env.WEBHOOK_SECRET,
-  cert: process.env.PRIVATE_KEY,
-  port: 3000,
-  webhookPath: '/webhooks/github',
-  webhookProxy: process.env.WEBHOOK_PROXY_URL
-})
+const probot = new Probot({})
+
 probot.load(app)
-probot.start()
