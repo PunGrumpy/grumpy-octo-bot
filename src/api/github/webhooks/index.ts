d05d03b61d236aa46456f2b1ef5c993f7a6e691b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-11 22:14:07 +0700

    :bug: api vercel

--- src/api/github/webhooks/index.ts
+++ src/api/github/webhooks/index.ts
@@ -1,7 +1,13 @@
 import { createNodeMiddleware, createProbot } from 'probot'
 import app from '../../../index'
 
-module.exports = createNodeMiddleware(app, {
-  probot: createProbot(),
+const probot = createProbot({
+  id: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY || process.env.PRIVATE_KEY_PATH,
+  secret: process.env.WEBHOOK_SECRET
+})
+
+export default createNodeMiddleware(app, {
+  probot,
   webhooksPath: '/api/github/webhooks'
 })
