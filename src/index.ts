3e06156a42ba995196dcbaa776f99a6bf70b88b8
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-12 01:01:39 +0700

    :package: update all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Probot } from 'probot'
 import app from './app'
 
-const probot = new Probot({})
+const probot = new Probot({
+  id: process.env.APP_ID,
+  secret: process.env.WEBHOOK_SECRET,
+  cert: process.env.PRIVATE_KEY,
+  port: 3000,
+  webhookPath: '/webhooks/github',
+  webhookProxy: process.env.WEBHOOK_PROXY_URL
+})
 probot.load(app)
 probot.start()
