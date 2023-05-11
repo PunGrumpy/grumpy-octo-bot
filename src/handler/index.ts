a95ad7e7c2e4def24f52732af1cdfea954e032f2
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-05-12 01:52:10 +0700

    :bug: fix ts2590

--- src/handler/index.ts
+++
@@ -1,8 +0,0 @@
-import { Probot } from 'probot'
-import pullRequestHandler from './pullRequestHandler'
-import issueHandler from './issueHandler'
-
-export default (app: Probot): void => {
-  app.on('pull_request.opened', pullRequestHandler)
-  app.on('issues.opened', issueHandler)
-}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,33 @@
 import { Probot } from 'probot'
+import { Context } from 'probot'
 import pullRequestHandler from './handler/pullRequestHandler'
-import issueHandler from './handler/issueHandler'
-import discordWebhookHandler from './handler/discordWebhookHandler'
 import { logger } from './logger'
+import discordWebhookHandler from './handler/discordWebhookHandler'
+import issueHandler from './handler/issueHandler'
+
+const pullRequestHandle = async (
+  context: Context<'pull_request.opened'>
+): Promise<void> => {
+  try {
+    await pullRequestHandler(context)
+  } catch (err) {
+    logger.error(err)
+    discordWebhookHandler('An error occurred in pullRequestHandler', err)
+  }
+}
+
+const issueHandle = async (
+  context: Context<'issues.opened'>
+): Promise<void> => {
+  try {
+    await issueHandler(context)
+  } catch (err) {
+    logger.error(err)
+    discordWebhookHandler('An error occurred in issueHandler', err)
+  }
+}
 
 export = (app: Probot): void => {
-  app.on('pull_request.opened', async context => {
-    try {
-      await pullRequestHandler(context)
-    } catch (err) {
-      logger.error('Error processing pull request:', err)
-      await discordWebhookHandler('Error processing pull request', err)
-    }
-  })
-
-  app.on('issues.opened', async context => {
-    try {
-      await issueHandler(context)
-    } catch (err) {
-      logger.error('Error processing issue:', err)
-      await discordWebhookHandler('Error processing issue', err)
-    }
-  })
+  app.on('pull_request.opened', pullRequestHandle)
+  app.on('issues.opened', issueHandle)
 }
